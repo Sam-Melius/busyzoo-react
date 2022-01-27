9c8d950a6542cc95f81f8474e036e0f0c0b1e0ad
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-26 17:21:51 -0700

    set state of fighters

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+const [batSize, setBatSize] = useState(1);
+const [wolfSize, setWolfSize] = useState(1);
+
   return (
     <div className="App">
       <section className='fight'>
+        <div className='bat'>
+          <p> 🦇</p>
+        </div>
+        <div className='wolf'>
+          <p> 🐺</p>
+        </div>
 
       </section>
       <section className='sign'>
 
       </section>
       <section className='parade'>
-        
+
       </section>
     </div>
   );
