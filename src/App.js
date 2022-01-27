8ee54a6476598deb915be2bf209b7896b9bf60a3
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-26 17:33:14 -0700

    added button functionality to fighters

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { useState } from 'react';
 
 function App() {
 
-const [batSize, setBatSize] = useState(1);
-const [wolfSize, setWolfSize] = useState(1);
+  const [batSize, setBatSize] = useState(1);
+  const [wolfSize, setWolfSize] = useState(1);
+  const batStyle = {
+    fontSize: `${batSize}rem`
+  };
+  const wolfStyle = {
+    fontSize: `${wolfSize}rem`
+  };
 
   return (
     <div className="App">
       <section className='fight'>
         <div className='bat'>
-          <p> 🦇</p>
+          <p style={batStyle}> 🦇</p>
+          <button onClick={() => setBatSize(batSize + 1)}>Bat consumes blood and gets stronger!</button>
+          <button onClick={() => setWolfSize(wolfSize - 1)}>Bat bites wolf!!!</button>
         </div>
         <div className='wolf'>
-          <p> 🐺</p>
+          <p style={wolfStyle}> 🐺</p>
+          <button onClick={() => setWolfSize(wolfSize + 1)}>Its a full moon! Wolf gets stronger!!!</button>
+          <button onClick={() => setBatSize(batSize - 1)}>Wolf claws bat!!!</button>
         </div>
 
       </section>
